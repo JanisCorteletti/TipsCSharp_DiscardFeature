f151538d1515481d1a010ab690de7b81e2989c8e
Janis Leite Corteletti <janis.corteletti@gmail.com>
2021-02-17 20:00:06 +0100

    Add project files.

---
+++ TipsConsole/Examples.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Text;
+
+namespace TipsConsole
+{
+	internal class Examples
+	{
+		public void ShowExample()
+		{
+			int unusedVariable = 10;
+
+			// the underscore can serve as the discards placeholder
+			// think of a discard as a temporary, dummy variables
+			// that is intentionally unused in application code. 
+			// Discards are equivalent to unassigned variables.
+			// They do not have a value.
+
+
+			// assign an expression to a discard.
+			_ = 12 + 14;
+
+			// var x = _ ;  // cannot do this 
+
+			// available for
+			// Calls to methods with out parameters.
+			// tuple and object deconstruction
+			// pattern matching with "is" and "switch".
+
+			//DiscardOutParam();
+			 Deconstruct();
+
+		}
+		public void DiscardOutParam()
+		{
+
+			var inputs = new List<string> { "123", "234", "abc", "def" };
+
+			foreach (string input in inputs)
+			{
+				if (int.TryParse(input, out _))
+					Console.WriteLine($"'{input}': valid");
+				else
+					Console.WriteLine($"'{input}': invalid");
+			}
+		}
+
+		public void Deconstruct()
+		{
+			var robot = new Robot { Name = "X632", 
+															HasWheels = true, 
+															SensorCount = 12 };
+			var (a, b, c) = robot.GetStats();
+
+			var (x, y, _) = robot.GetStats();
+
+			Console.WriteLine("Robot Name: " + a);
+			Console.WriteLine("Robot HasWheels: " + b);
+			Console.WriteLine("Robot SensorCount: " + c);
+
+			Console.WriteLine("Robot Name: " + x);
+			Console.WriteLine("Robot HasWheels: " + y);
+			Console.ReadLine();
+		}
+
+	}
+	public class Robot
+	{
+		public string Name { get; set; }
+		public bool HasWheels { get; set; }
+		public int SensorCount { get; set; }
+		public (string, bool, int) GetStats()
+		{
+			return (Name, HasWheels, SensorCount);
+		}
+	}
+}
